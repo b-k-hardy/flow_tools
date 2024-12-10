4cbce715479c0b78f2136aa3c0b0b1e7e9a7dcd9
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2024-12-10 09:30:06 -0500

    swap around driver code

--- driver_code.py
+++ driver_code.py
@@ -8,6 +8,34 @@ import patient4Dflow.patient_class
 logger = logging.getLogger(__name__)
 
 
+def full_run(patient_id: str, data_path: str, seg_path: str) -> None:
+    """Run full analysis pipeline for a single patient.
+
+    This function can be imported directly into the driver code to run the full analysis pipeline. Additionally, this
+    function can be used as inspiration to show how the sub-functions are generally ordered. Of note:
+    1. The first step is always to initialize the Patient4DFlow object.
+    2. For any analysis, a skeleton will need to be added
+    3. Exporting the velocity data to VTK format is a common step, but not required
+    4. Exporting the data to .mat MATLAB format is required for the STE pressure drop estimation
+    5. The STE pressure drop estimation is a key part of the analysis and required for the remaining steps
+    6. Exporting the pressure field to VTK format is optional, but can be useful for visualization
+    7. Plotting the pressure drop over time is a key part of the analysis
+
+    Args:
+        patient_id (str): Anonymized patient ID
+        data_path (str): Absolute path to patient data directory
+        seg_path (str): Relative path from data_path to segmentation file
+
+    """
+    patient = patient4Dflow.patient_class.Patient4DFlow(patient_id, data_path, seg_path)
+    patient.add_skeleton()
+    patient.export_vel_to_vti()
+    patient.export_to_mat()
+    patient.get_ste_drop()
+    patient.export_p_field()
+    patient.plot_dp()
+
+
 def main() -> None:
     """Run and log analysis of 4D flow data."""
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
