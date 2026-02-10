9f512e01bcd931f2ac9727458e559834a189d9c7
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2026-02-10 13:12:59 -0500

    remove old matlabengine functions

--- patient4Dflow/export_masks.m
+++
@@ -1,9 +0,0 @@
-function export_masks(save_path, mask, inlet, outlet)
-% EXPORT_MASKS  Export segmentation data in Python class to MATLAB
-%   Note that this function purely exists to be called from Python
-%   Output: None
-%   Input:  mask, inlet, and outlet from 3D Slicer
-
-save(save_path, "mask", "inlet", "outlet", "-v7.3")
-
-end
