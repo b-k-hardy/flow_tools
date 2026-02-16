7dc5a4a839af7f48c9318dcbd1ff7f4c0f0a2b4c
Brandon Hardy <59355744+b-k-hardy@users.noreply.github.com>
2026-02-15 21:47:07 -0500

    big update to read_dicoms.py as standalone script

--- patient4Dflow/read_dicoms.py
+++ patient4Dflow/read_dicoms.py
@@ -1,15 +1,18 @@
 """Module for reading in DICOM files and converting them to numpy arrays."""
 
-from __future__ import annotations
-
 import logging
 import os
+import sys
 from pathlib import Path
 
+import h5py
 import matplotlib.pyplot as plt
 import nrrd
 import numpy as np
 import pydicom
+import pyvista as pv
+import scipy.io as sio
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +36,7 @@ def import_segmentation(seg_path: str) -> tuple[np.ndarray, np.ndarray, np.ndarr
     return segmentation, origin, spacing
 
 
-def import_flow(
+def import_phase(
     paths: tuple[str, str, str],
     vencs: tuple[int, int, int],
     phase_range: int = 4096,
@@ -251,7 +254,7 @@ def import_ssfp(dicom_path: str, check: None | int = None) -> np.ndarray:
 
     # create 3D array
     img_shape = list(slices[0].pixel_array.shape)
-    nz = int(len(slices))
+    nz = len(slices)
     img_shape.append(nz)
     img4d = np.zeros((img_shape[0], img_shape[1], nz))
 
@@ -312,7 +315,7 @@ def find_dicom_dirs(dir_path: str) -> tuple[list[str], list[str]]:
         # I NEED TO CHECK FOR SSFP IN SERIES DESCRIPTION AND THEN IF SSFP IS FOUND I WILL CHECK
         # FOR SLICE THICKNESS TO GRAB ACTUAL DATA... huh
         # there can be multiple studies; therefore we should check for SeriesNumber as well
-        if "4dflow" in check_file.SeriesDescription.lower() and hasattr(
+        if ("4d" and "flow") in check_file.SeriesDescription.lower() and hasattr(
             check_file,
             "SequenceName",
         ):
@@ -346,7 +349,26 @@ def import_all_dicoms(dir_path: str) -> tuple[np.ndarray, np.ndarray]:
         tuple[np.ndarray, np.ndarray]: magnitude data, flow data
 
     """
-    wip_list, ssfp_list = find_dicom_dirs(dir_path)
+    # wip_list, ssfp_list = find_dicom_dirs(dir_path)
+    # NOTE: I THINK I CAN REWRITE THIS TO DO IMPORTING BASED ON THE FILEPATH ONLY
+    wip_fname_list = [
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/0000B33F/EE0A2F30",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/000004A5/EE0A6D97",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/000051BA/EE0A3B55",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/0000215E/EE0A7CFB",
+    ]
+
+    wip_dir_list = [
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/0000B33F/",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/000004A5/",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/000051BA/",
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5/0000215E/",
+    ]
+
+    wip_file_list = [pydicom.dcmread(wip_dir) for wip_dir in wip_fname_list]
+    wip_list = [{"wip": wip.SequenceName, "dir": wip_dir_list[wip_file_list.index(wip)]} for wip in wip_file_list]
+
+    ssfp_list = []
 
     # stupid ass initialization so linter doesn't freak out
     vencs = np.zeros(3)
@@ -358,16 +380,16 @@ def import_all_dicoms(dir_path: str) -> tuple[np.ndarray, np.ndarray]:
         wip = item["wip"]
 
         if wip[-2:] == "in":  # or rl?
-            flow_paths[0] = dir_path / dir_name
+            flow_paths[0] = dir_name
             vencs[0] = int(wip[-5:-2])
         elif wip[-2:] == "ap":
-            flow_paths[1] = dir_path / dir_name
+            flow_paths[1] = dir_name
             vencs[1] = int(wip[-5:-2])
         elif wip[-2:] == "fh":
-            flow_paths[2] = dir_path / dir_name
+            flow_paths[2] = dir_name
             vencs[2] = int(wip[-5:-2])
         else:
-            mag_path = dir_path / dir_name
+            mag_path = dir_name
 
     # NOTE: what the hell is this... I'm just overwriting with the most recent dir_name
     for item in ssfp_list:
@@ -375,7 +397,189 @@ def import_all_dicoms(dir_path: str) -> tuple[np.ndarray, np.ndarray]:
         ssfp_path = dir_name
 
     mag_data = import_mag(mag_path)
-    ssfp_data = import_ssfp(dir_path / ssfp_path)
-    flow_data, dx, dt = import_flow(flow_paths, vencs)
+    # ssfp_data = import_ssfp(dir_path / ssfp_path)
+    ssfp_data = []
+    flow_data, dx, dt = import_phase(flow_paths, vencs)
 
     return mag_data, ssfp_data, flow_data, dx, dt
+
+
+def import_flow(flow_paths: tuple[Path, Path, Path, Path]) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.floating]:
+    dicom_objects = [pydicom.dcmread(dcm_file) for dcm_file in flow_paths]
+    wip_list = [{"wip": image.SequenceName, "dir": Path(image.filename).parent} for image in dicom_objects]
+
+    vencs = np.zeros(3)
+    phase_paths = [""] * 3
+    mag_path = ""
+
+    for item in wip_list:
+        dir_name = item["dir"]
+        wip = item["wip"]
+
+        if wip[-2:] == "in":  # or rl?
+            phase_paths[0] = dir_name
+            vencs[0] = int(wip[-5:-2])
+        elif wip[-2:] == "ap":
+            phase_paths[1] = dir_name
+            vencs[1] = int(wip[-5:-2])
+        elif wip[-2:] == "fh":
+            phase_paths[2] = dir_name
+            vencs[2] = int(wip[-5:-2])
+        else:
+            mag_path = dir_name
+
+    mag_data = import_mag(mag_path)
+    flow_data, dx, dt = import_phase(phase_paths, vencs)
+
+    return mag_data, flow_data, dx, dt
+
+
+def _export_to_mat(
+    data_id: str,
+    flow_data: np.ndarray,
+    mask: np.ndarray,
+    dx: np.ndarray,
+    dt: np.floating,
+    res: np.ndarray,
+) -> None:
+    """Export flow velocity struct and mask data to MATLAB .mat files.
+
+    Args:
+        output_dir (None | str, optional): Path to mat output directory. Defaults to None to autogenerate directory.
+
+    """
+    # navigate MATLAB instance to current working directory to call custom function
+    logger.info("Exporting MATLAB velocity structs...")
+
+    vx = flow_data[0, :, :, :, :].copy()
+    vy = flow_data[1, :, :, :, :].copy()
+    vz = flow_data[2, :, :, :, :].copy()
+    vx_dict = {"im": vx, "PixDim": dx, "dt": dt, "res": res}
+    vy_dict = {"im": vy, "PixDim": dx, "dt": dt, "res": res}
+    vz_dict = {"im": vz, "PixDim": dx, "dt": dt, "res": res}
+    # this weird format is to make sure the struct is preserved for MATLAB
+    vel_output = {"v": np.array([vx_dict, vy_dict, vz_dict], dtype=object).T}
+
+    sio.savemat(f"../data/{data_id}/mat/{data_id}_vel.mat", vel_output)
+
+    logger.info("Exporting MATLAB mask...")
+    sio.savemat(f"../data/{data_id}/mat/{data_id}_mask.mat", {"mask": mask})
+
+
+def _export_to_h5(
+    data_id: str,
+    flow_data: np.ndarray,
+    mask: np.ndarray,
+    dx: np.ndarray,
+    dt: np.floating,
+    res: np.ndarray,
+) -> None:
+    """Export flow velocity and mask data to HDF5 format.
+
+    Args:
+        output_dir (None | str, optional): Path to h5 output directory. Defaults to None to autogenerate directory.
+
+    """
+    logger.info("Exporting HDF5 velocity...")
+
+    with h5py.File(f"../data/{data_id}/h5/{data_id}_vel.h5", "w") as f:
+        f.create_dataset("v", data=flow_data)
+        f.create_dataset("dx", data=dx)
+        f.create_dataset("dt", data=dt)
+        f.create_dataset("res", data=res)
+
+    logger.info("Exporting HDF5 mask...")
+    with h5py.File(f"../data/{data_id}/h5/{data_id}_mask.h5", "w") as f:
+        f.create_dataset("mask", data=mask)
+
+
+def _export_to_vti(
+    data_id: str,
+    flow_data: np.ndarray,
+    mask: np.ndarray,
+    dx: np.ndarray,
+    dt: np.floating,  # unused, but keeping in API just in case I ever add time series info
+    res: np.ndarray,
+) -> None:
+    """Export flow velocity data to VTK ImageData format.
+
+    Args:
+        output_dir (None | str, optional): Path to vti output directory. Defaults to None to autogenerate directory.
+
+    """
+    logger.info("Exporting to VTI...")
+    for t in tqdm(range(res[-1])):
+        # write velocity field one timestep at a time
+        vel = (flow_data[:, :, :, :, t] * mask).reshape(3, -1, order="F").T
+
+        out_path = f"../data/{data_id}/vti/{data_id}_flow_{t:03d}.vti"
+
+        frame = pv.ImageData(dimensions=(res[:-1] + 1), spacing=dx)
+        frame.cell_data["Velocity"] = vel
+        frame.save(out_path)
+
+
+def export_data(
+    data_id: str,
+    flow_data: np.ndarray,
+    mask: np.ndarray,
+    dx: np.ndarray,
+    dt: np.floating,
+    res: np.ndarray,
+    filetype: tuple[str, str, str] = ("mat", "h5", "vti"),
+) -> None:
+    output_dir = f"../data/{data_id}"
+    # make sure output path exists, create directory if not
+    base_data_path = Path(output_dir)
+    if "mat" in filetype:
+        mat_output_dir = base_data_path / "mat"
+        mat_output_dir.mkdir(parents=True, exist_ok=True)
+        _export_to_mat(data_id, flow_data, mask, dx, dt, res)
+    if "h5" in filetype:
+        h5_output_dir = base_data_path / "h5"
+        h5_output_dir.mkdir(parents=True, exist_ok=True)
+        _export_to_h5(data_id, flow_data, mask, dx, dt, res)
+    if "vti" in filetype:
+        vti_output_dir = base_data_path / "vti"
+        vti_output_dir.mkdir(parents=True, exist_ok=True)
+        _export_to_vti(data_id, flow_data, mask, dx, dt, res)
+
+
+def main():
+    # set up logging to print to stdout
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+    # NOTE: THESE ARE THINGS YOU CAN CHANGE ########################################################################
+    data_id = "CR"
+    base_dicom_directory = (
+        "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/4DFlow/DICOM/00009AD4/AA9D5841/AAD51FE5"
+    )
+    dicom_extensions = ["0000B33F/EE0A2F30", "000004A5/EE0A6D97", "000051BA/EE0A3B55", "0000215E/EE0A7CFB"]
+    seg_path = "/Users/bkhardy/Downloads/Dissection_2025_karolinska/CR_20250611/Segmentation_flow.nrrd"
+    ################################################################################################################
+
+    # Main reader and exporter
+    flow_paths = tuple([Path(f"{base_dicom_directory}/{filename}") for filename in dicom_extensions])
+    mag_data, flow_data, dx, dt = import_flow(flow_paths)
+    flow_data = np.flip(flow_data, axis=0)
+    flow_data[0] *= -1
+    flow_data = flow_data.copy()  # make sure it's contiguous in memory prior to export
+    res = np.array(mag_data.shape)
+
+    segmentation, seg_origin, seg_spacing = import_segmentation(seg_path)
+    segmentation = np.transpose(segmentation, (1, 0, 2))
+    segmentation = np.flip(segmentation, axis=2)
+
+    export_data(
+        data_id,
+        flow_data,
+        segmentation,
+        dx,
+        dt,
+        res,
+        filetype=("mat", "h5", "vti"),
+    )
+
+
+if __name__ == "__main__":
+    main()
